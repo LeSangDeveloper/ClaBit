59703f96beb3486ce94a697cb1c874c6bad72d4e
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2022-10-05 09:06:17 +0800

    remove redundancy file

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('versions', {
-    node: () => process.versions.node,
-    chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron,
-    checkEnv: () => ipcRenderer.invoke('check-env')
-})
